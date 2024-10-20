da72b8e28b0b56243445fc1f8ef8a2fe74a7ca16
윤설이 <olerlmin1234@gmail.com>
2024-10-20 16:33:25 +0900

    while 문으로 고쳤을 때

--- AlgorithmsTest/src/star/star2.java
+++ AlgorithmsTest/src/star/star2.java
@@ -3,7 +3,22 @@ package star;
 public class star2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int size = 5;
+		int floor = 1;
+		
+		while(floor <= size) {
+			
+			int star = 0;
+			while (star < floor) {
+				System.out.print("*");
+				star++;
+			}
+			
+			System.out.println();
+			floor ++;
+	
+		}
+		
 
 	}
 
